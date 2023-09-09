24c7e609564268d06504183e12e6cfdf92ccc2f9
lmaodor666 <milicevicviktor@gmail.com>
2023-09-09 20:02:39 +0200

    first commit

---
+++ vmdocs-main/theme.config.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>VM/chat</span>,
+  project: {
+    link: 'https://github.com/vmlabss/vmchat',
+ 
+  
+  },
+  docsRepositoryBase: 'https://github.com/vmlabss',
+  footer: {
+    text: 'vm labs docs',
+  },
+}
+
+export default config
